1c45e292804fae3f1633d3dd67dbd9a8099b862c
MTPE <mtpe2301@gmail.com>
2025-02-20 11:45:05 +0700

    2

--- vite.config.ts
+++ vite.config.ts
@@ -8,3 +8,14 @@ export default defineConfig({
 		port: 3000,
 	},
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+// 	plugins: [react()],
+// 	server: {
+// 	  host: '0.0.0.0',
+// 	  cors: true,
+// 	},
+// });
